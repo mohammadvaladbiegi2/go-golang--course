3b6c321d6dbc2b6e7c990a489838e622fb0aac32
mohammadvaladbiegi2 <126916758+mohammadvaladbiegi2@users.noreply.github.com>
2025-03-26 14:30:31 +0330

    todo updating

--- projects/todo-cli/main.go
+++ projects/todo-cli/main.go
@@ -2,42 +2,82 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type User struct {
-	ID       string
+	ID       int
 	email    string
 	password string
 }
 
-var categorys []string
 var userStoreg []User
 
+type Task struct {
+	ID       int
+	Title    string
+	DouDate  string
+	Category string
+	IsDone   bool
+	UserID   int
+}
+
+type CategoryStruct struct {
+	title  string
+	UserID int
+}
+
+var categorys []CategoryStruct
+var taskStoreg []Task
+var userID int
+
+const UserStoragePath = "user-storage.txt"
+
 func main() {
+	loadUsers()
 	command := flag.String("command", "no-command", "create a command")
 	flag.Parse()
-
+	authentications()
+	if userID == 0 {
+		authentications()
+	}
 	for {
-		runcommand(*command)
 		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Println("Please enter the new command")
+		fmt.Println("Please enter the new command or tab to see list of command")
 		scanner.Scan()
 		*command = scanner.Text()
+		runcommand(*command)
 	}
 
 }
 
+func cerateFile(user string) {
+
+	file, err := os.OpenFile(UserStoragePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer file.Close() // این کد در اخر تابع اجرا میشه
+
+	if err != nil {
+		fmt.Println("خطا در ایجاد فایل:", err)
+		return
+	}
+
+	_, err = file.WriteString(user)
+	file.Close()
+	if err != nil {
+		fmt.Println("خطا در نوشتن فایل:", err)
+	}
+}
+
 func runcommand(command string) {
 	switch command {
-	case "creaet-task":
+	case "create-task":
 		createTask()
-	case "register":
-		register()
-	case "login":
-		login()
 	case "create-category":
 		createCategory()
 	case "list-category":
@@ -47,8 +87,6 @@ func runcommand(command string) {
 	default:
 		fmt.Println("Enter a valid command\nList of commands:")
 		fmt.Println("create-task => Create a new task")
-		fmt.Println("register => Sign up a new user")
-		fmt.Println("login => Log in to your account")
 		fmt.Println("create-category => Create a new category")
 		fmt.Println("list-category => List all categories")
 		fmt.Println("exit => Exit the application")
@@ -57,12 +95,11 @@ func runcommand(command string) {
 
 func createCategory() {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Please enter the category of task")
+	fmt.Println("create a  category for task")
 	scanner.Scan()
-	category := scanner.Text()
-	categorys = append(categorys, category)
+	category_title := scanner.Text()
+	categorys = append(categorys, CategoryStruct{title: category_title, UserID: userID})
 	fmt.Println("Category created successfully ✅✅")
-	fmt.Printf("List of categories => %v\n", categorys)
 }
 
 func listCategories() {
@@ -70,6 +107,9 @@ func listCategories() {
 }
 
 func createTask() {
+	if len(categorys) == 0 {
+		createCategory()
+	}
 	scanner := bufio.NewScanner(os.Stdin)
 	var title_task string
 	var category string
@@ -79,22 +119,59 @@ func createTask() {
 	title_task = scanner.Text()
 
 	fmt.Println("Please enter the category of the task")
+	fmt.Println("list of categorys => ")
+	for _, value := range categorys {
+		fmt.Printf("%s  ", value.title)
+	}
+	fmt.Println("")
 	scanner.Scan()
 	category = scanner.Text()
+	for {
+		if isValidCategory(scanner.Text()) {
+			break
+		}
+		fmt.Println("Please enter the valid category")
+		fmt.Println("list of categorys => ")
+		for _, value := range categorys {
+			fmt.Printf("%s  ", value.title)
+		}
+		scanner.Scan()
+		category = scanner.Text()
+
+	}
 
 	fmt.Println("Please enter the due date of the task")
 	scanner.Scan()
 	doudate = scanner.Text()
 
-	fmt.Printf("Task created with this info: title => %s, category => %s, due date => %s\n", title_task, category, doudate)
+	task := Task{
+		ID:       len(taskStoreg) + 1,
+		Title:    title_task,
+		DouDate:  doudate,
+		Category: category,
+		IsDone:   false,
+		UserID:   userID,
+	}
+	taskStoreg = append(taskStoreg, task)
+	fmt.Printf("Task created with this info: title => %s, category => %s, due date => %s\n, ID of creator => %d\n", title_task, category, doudate, userID)
 }
 
-func register() {
+func isValidCategory(category string) bool {
+	for _, value := range categorys {
+		if value.title == category {
+			return true
+		}
+	}
+
+	return false
+}
+
+func authentications() {
 	scanner := bufio.NewScanner(os.Stdin)
 	var email string
 	var password string
-	var id string
-	fmt.Println("Signup\nPlease enter the email")
+	var id int
+	fmt.Println("Signup / login \nPlease enter the email")
 	scanner.Scan()
 	email = scanner.Text()
 
@@ -102,30 +179,66 @@ func register() {
 	scanner.Scan()
 	password = scanner.Text()
 
-	id = email
-	userStoreg = append(userStoreg, User{email: email, ID: id, password: password})
-
-	fmt.Println("welcom")
-}
-
-func login() {
-	scanner := bufio.NewScanner(os.Stdin)
-	var email string
-	var password string
-	fmt.Println("Signup\nPlease enter the email")
-	scanner.Scan()
-	email = scanner.Text()
-
-	fmt.Println("Please enter the password ")
-	scanner.Scan()
-	password = scanner.Text()
-
-	for _, v := range userStoreg {
-		if v.email == email && v.password == password {
+	for _, user := range userStoreg {
+		if user.email == email && user.password == hashPassword(password) {
+			id = len(userStoreg) + 1
+			userID = id
 			fmt.Println("welcom")
+			return
+		} else if user.email == email || user.password == hashPassword(password) {
+			fmt.Println("user or password not crect try agin")
 			return
 		}
 	}
 
-	fmt.Println("user not found please register first")
+	id = len(userStoreg) + 1
+	userID = id
+	user := fmt.Sprintf("ID : %d,email : %s,password : %s\n", id, email, hashPassword(password))
+	cerateFile(user)
+	fmt.Println("welcom")
+}
+
+func loadUsers() {
+	file, err := os.Open(UserStoragePath)
+	if err != nil {
+		fmt.Println("خطا در باز کردن فایل:", err)
+		return
+	}
+	defer file.Close() // این کد در اخر تابع اجرا میشه
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ",")
+		if len(parts) != 3 {
+			fmt.Println("خطا در پردازش خط:", line)
+			continue
+		}
+
+		idStr := strings.TrimPrefix(parts[0], "ID : ")
+		email := strings.TrimPrefix(parts[1], "email : ")
+		password := strings.TrimPrefix(parts[2], "password : ")
+
+		id, err := strconv.Atoi(strings.TrimSpace(idStr))
+		if err != nil {
+			fmt.Println("خطا در تبدیل ID:", err)
+			continue
+		}
+
+		user := User{
+			ID:       id,
+			email:    strings.TrimSpace(email),
+			password: strings.TrimSpace(password),
+		}
+		userStoreg = append(userStoreg, user)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("خطا در خواندن فایل:", err)
+	}
+}
+
+func hashPassword(text string) string {
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
 }
